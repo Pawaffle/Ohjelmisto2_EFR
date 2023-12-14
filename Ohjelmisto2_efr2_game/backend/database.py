df2659a6b28d965976c468c683ee5b322adc943e
andreivl23 <127105524+andreivl23@users.noreply.github.com>
2023-12-14 11:55:05 +0200

    Update database.py

--- Ohjelmisto2_efr2_game/backend/database.py
+++ Ohjelmisto2_efr2_game/backend/database.py
@@ -3,7 +3,7 @@ import mysql.connector
 
 def make_connection():
     connection = mysql.connector.connect(
-             host='172.232.129.9',
+             host='',
              port=3306,
              database='efr_mini',
              user='American',
